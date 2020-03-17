68e04ea0c66d9e754bb37399a304665be14a5341
desdelgado <davidesmoleydelgado@gmail.com>
2020-03-17 11:47:11 -0500

    Successfully tested package for input path and output path

---
+++ test_path.py
@@ -0,0 +1,14 @@
+from rheodata.extractors.antonpaar import AntonPaarExtractor
+
+extractor = AntonPaarExtractor()
+
+path = "/Users/Delgado/Documents/Research/rheology-data-toolkit/rheodata/extractors/test_data/excel_test_data/Steady State Viscosity Curve-LO50C_excel.xlsx"
+# Enter the realtive path to the folder you wish to output your data to
+output_folder_path = "/Users/Delgado/Documents/Research/chimad_project/rheodata/extractors/test_data/"
+
+test = extractor.import_rheo_data(path, output_folder_path)
+
+#test = pd.read_excel(path)
+
+
+print(test)
