b68b15c79d54ddcb711dc936b713d8cad38a3855
desdelgado <davidesmoleydelgado@gmail.com>
2020-06-22 16:23:27 -0500

    Added dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rheodata", 
-    version="0.0.5",
+    version="0.0.6",
     author="David Delgado",
     author_email="daviddelgado2020@u.northwestern.com",
     description="Packge to help process rheology data",
@@ -17,4 +17,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'h5py>=2.10.0',
+        'tables>=3.6.1',
+        'jsonschema>=3.2.0',
+        'pickleshare>=0.7.5',
+        'pandas>=1.0.4',
+        'xlrd>=1.2.0'
+    ]
 )
